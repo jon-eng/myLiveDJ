789fb732155edb1aaa1079b524b82f11be224326
Jonathan Eng <jon.kh.eng@gmail.com>
2017-04-13 16:26:44 -0400

    added youtube landing video & soundcloud player

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 $(function(){
   console.log("app loaded")
+    $(window).scroll(function() {              
+      // $('.soundcloud').toggle($(document).scrollTop() > 100);
+      $(document).scrollTop() > 100 ? $('.soundcloud:hidden').fadeIn() : $('.soundcloud:visible').fadeOut();
+    });
+
+    var bg_video =  iframe.getElementById('bg-video'); 
+    bg_video.mute();
+    console.log(bg_video)
+
 });
